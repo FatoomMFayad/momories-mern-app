c0529bdb11ae4a8a61dbbb96b6c1479a70a0fe34
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-08-19 15:08:46 +0300

    server

--- server/index.js
+++ server/index.js
@@ -2,3 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+//express 4.16+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+
+app.use(cors());
+
