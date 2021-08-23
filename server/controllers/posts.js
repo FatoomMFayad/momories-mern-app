d3925c895f57cd6da8ac3388afa17bb649b4e5b6
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-08-23 14:48:28 +0300

    getPosts and createPost functions

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,24 @@
-export const getPosts = ((req, res)=>{
-    res.send('This Works');
-})
+import PostMessage from '../models/postMessage.js';
+
+export const getPosts = (async(req, res)=>{
+    try{
+        const postMessages = await PostMessage.find();
+        console.log(postMessages);
+        res.status(200).json(postMessages);
+    }catch(error){
+        res.status(404).json({messge: error.messge});
+    }
+});
+
+export const createPost= async(req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+    try{
+        await newPost.save();
+        res.status(201).json(newPost);
+
+    }catch(error){
+        res.status(409).json({messge: error.messge});
+    }
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { getPosts} from '../controllers/posts.js'
+import { createPost} from '../controllers/posts.js'
 
 const router = express.Router();
 
 router.get('/', getPosts);
+router.get('/', createPost);
 
 export default router;
