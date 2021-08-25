29a82935de275e52857e87a8df12071539f7c531
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-08-25 13:59:56 +0300

    add memory form

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://ffatoom:eng17@5@1988l@cluster0.fuput.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = '';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
