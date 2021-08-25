14c331edb8a5dd3db9c45f4349eb7d0f65737926
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-08-25 10:10:19 +0300

    add styles and front end api

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
-
+import useStyles from './styles';
 const App = () => {
+    const classes = useStyles();
     return(
         <Container maxwidth='lg'>
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memories" heigth="60"/>
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60"/>
             </AppBar>
             <Grow in>
                 <Container>
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import Post from './Post/Post.js'
+import Post from './Post/Post.js';
+import useStyles from './styles';
 const Posts = () => {
+    const classes = useStyles();
     return (
         <>
             <h1>POSTS</h1>
