bfb057064350fc73e00608a13486a407eb41017d
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-08-25 12:19:21 +0300

    add actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 import memories from './images/memories.png';
+
+import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
 import useStyles from './styles';
+
 const App = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect( () => {
+        dispatch(getPosts());
+    }, [dispatch]);
     return(
         <Container maxwidth='lg'>
             <AppBar className={classes.appBar} position="static" color="inherit">
