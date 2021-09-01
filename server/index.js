d321e2e9a629b3df0f9723cea4030c8b2c5e22ca
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-09-01 15:06:56 +0300

    Post Card

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = '';
-const PORT = process.env.PORT|| 7000;
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
