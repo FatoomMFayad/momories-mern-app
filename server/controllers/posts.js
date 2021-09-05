5e337c0adad76fadef430d79cfa3780b97f35e1a
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-09-05 11:09:26 +0300

    bug fix

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -26,13 +26,15 @@ export const createPost= async(req, res) => {
     }
 }
 
-export const updatePost= async(req, res) => {
-    const { id: _id } = req.params;
-    const post = req.body;
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
     
-    if(!mongoose.Types.objectId.isValid(_id))  res.status(404).send('No post with that id');
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new : true });
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
 
     res.json(updatedPost);
 }
