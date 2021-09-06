8f83b761a2b4054dbeb3e9354af16560b13ba180
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-09-06 13:09:45 +0300

    env file

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 const PORT = process.env.PORT||5000;
 const CONNECTION_URL =process.env.CONNECTION_URL;
+
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
