76f363f030eb99ddfd79d4667b3d9f1bcfc13ce2
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-09-06 13:08:15 +0300

    env file

--- server/index.js
+++ server/index.js
@@ -3,20 +3,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
-const CONNECTION_URL = '';
-const PORT = process.env.PORT|| 5000;
-
+const PORT = process.env.PORT||5000;
+const CONNECTION_URL =process.env.CONNECTION_URL;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
